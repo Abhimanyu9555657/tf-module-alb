f5146766b5c883f4d8847cb3c0856a4d01cfca2e
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-03 15:26:42 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_lb" "main" {
-  name               = "$(var.env)-alb"
+  name               = "${var.env}-alb"
   internal           = var.internal
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-  tags = merge(local.tags, { Name = "${var.env}-alb" })
+  tags               = merge(local.tags, { Name = "${var.env}-alb" })
 }
 
 resource "aws_security_group" "main" {
